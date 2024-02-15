55e841ec6e9d1198d46c872b14ec012506604b40
NihadGasimli <vip.ni.ko@mail.ru>
2024-02-16 02:40:26 +0400

    Finish

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,35 +1,101 @@
 const buttons = document.querySelector("#buttons");
-const btn = document.querySelectorAll(".btn");
+let btn = document.querySelectorAll(".btn");
+const gifs = document.querySelector("#gifs");
+let input;
 
-var input;
+var a = true;
 
 document.querySelector("#addBtn").addEventListener("click", function () {
-    input = document.querySelector("#addInp")
+    input = document.querySelector("#addInp");
     if (input.value.trim() !== "") {
-        var x = document.createElement("button")
-        x.classList.add("btn")
-        x.innerHTML = input.value
-        buttons.append(x)
-    }
-})
+        const newButton = document.createElement("button");
+        const newButtonX = document.createElement("button");
+        newButton.classList.add("newButtonClass")
+        newButton.classList.add("btn");
 
-for (let i of btn) {
-    i.onclick = function () {
-        var animalName = i.innerHTML
-        console.log(animalName)
+        newButtonX.style.display = "none"
+        newButtonX.id = "myau"
+
+        newButton.addEventListener("mouseenter", function () {
+            newButtonX.style.display = "block"
+            newButtonX.textContent = "X"
+        })
+        newButton.addEventListener("mouseleave", function () {
+            newButtonX.style.display = "none"
+            newButtonX.textContent = "X"
+        })
+
+        newButtonX.addEventListener("click", function () {
+            a = false;
+            buttons.removeChild(newButton)
+            setTimeout(function () {
+                a = true;
+            }, 1)
+        })
+
+        newButton.innerHTML = input.value;
+        input.value = "";
+        newButton.appendChild(newButtonX)
+        buttons.append(newButton);
+    }
+});
+
+function selectAllButtons() {
+    btn = document.querySelectorAll(".btn");
+    for (let i of btn) {
+        i.onclick = function () {
+            gifs.innerHTML = "";
+            for (let j in i.classList) {
+                if (i.classList[0] === 'newButtonClass') {
+                    var array = i.textContent.split("");
+                    array.pop();
+                    var d = array.join("")
+                    var animalName = d;
+                }
+                else if (i.classList[0] === "btn") {
+                    var animalName = i.textContent;
+                }
+
+            }
+            console.log(animalName);
+            main(animalName);
+        };
     }
 }
 
-// function api() {
-//     var url = fetch("https://api.giphy.com/v1/gifs/search?api_key=IunDZRBqAiaiIulBudp4Elej0Y5e6ScO&q=cat&limit=50&offset=25&rating=g&lang=en&bundle=messaging_non_clips", {
-//         method: "GET"
-//     }).then(
-//         function (url) {
-//             return url.json()
-//         }
-//     ).then(
-//         function () {
+function main(animalName) {
+    if (a) {
+        fetch(`https://api.giphy.com/v1/gifs/search?api_key=IunDZRBqAiaiIulBudp4Elej0Y5e6ScO&q=${animalName}&limit=50&offset=25&rating=&lang=en&bundle=messaging_non_clips`, {
+            method: "GET"
+        }).then(
+            function (response) {
+                return response.json();
+            }
+        ).then(
+            function (url) {
+                for (let i in url.data) {
+                    const h1 = document.createElement("p");
+                    const img = document.createElement("img");
+                    const gifDiv = document.createElement("div");
 
-//         }
-//     );
-// }
+                    img.style.border = "2px solid black";
+                    h1.innerHTML = `Rating: ${url.data[i].rating}`;
+                    img.src = `https://media1.giphy.com/media/${url.data[i].id}/giphy_s.gif`;
+
+                    img.addEventListener("click", function () {
+                        if (img.src === `https://media1.giphy.com/media/${url.data[i].id}/giphy_s.gif`) {
+                            img.src = `https://media1.giphy.com/media/${url.data[i].id}/giphy.gif`;
+                        } else {
+                            img.src = `https://media1.giphy.com/media/${url.data[i].id}/giphy_s.gif`;
+                        }
+                    });
+
+                    gifDiv.append(h1, img);
+                    gifs.append(gifDiv);
+                }
+            }
+        );
+    }
+}
+
+setInterval(selectAllButtons, 500);
